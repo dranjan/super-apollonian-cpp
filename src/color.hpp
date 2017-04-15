430741b1f4df30acf8d22f9e7efd51821119d14c
Darsh Ranjan <darsh.ranjan@here.com>
2017-04-15 12:30:58 -0700

    Implement missing functions

--- src/color.hpp
+++ src/color.hpp
@@ -27,6 +27,22 @@ inline RGBColor::RGBColor(double r, double g, double b)
 {
 }
 
+inline RGBColor::RGBColor(uint32_t h) {
+    b_ = (h & 0xff) / 255.0;
+    h >>= 8;
+    g_ = (h & 0xff) / 255.0;
+    h >>= 8;
+    r_ = (h & 0xff) / 255.0;
+}
+
+inline RGBColor
+RGBColor::blend(const RGBColor& other, double alpha) const {
+    double beta = 1.0 - alpha;
+    return RGBColor(r_*beta + other.r_*alpha,
+                    g_*beta + other.g_*alpha,
+                    b_*beta + other.b_*alpha);
+}
+
 } // apollonian
 
 #endif // COLOR_HPP
