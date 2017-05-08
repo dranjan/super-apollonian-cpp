45ed601b2223ad2e7ca2e1b3ad8d90669610049a
Darsh Ranjan <darsh.ranjan@here.com>
2017-05-07 21:23:30 -0700

    Use square pixels
    Previously, we used circular (overlapping) pixels, which do pretty well
    but result in a slightly less sharp image.

--- src/graphics.cpp
+++ src/graphics.cpp
@@ -4,40 +4,159 @@
 
 using apollonian::RGBColor;
 
+using std::min;
+using std::max;
+using std::acos;
+using std::asin;
+using std::sqrt;
+using std::ceil;
+using std::floor;
+using std::sin;
+
 namespace {
 
 inline double square(double x) {
     return x*x;
 }
 
+/* Compute the area of the intersection of the first quadrant of a
+ * circular disk centered at the origin and an axis-aligned rectangle.
+ */
+double circle_quadrant_area(
+        double rr,  /* square of radius */
+        double x0, double y0, double x1, double y1)
+{
+    if (x1 <= 0 || y1 <= 0) {
+        return 0;
+    }
+
+    x0 = max(x0, 0.0);
+    y0 = max(y0, 0.0);
+
+    double x0x0 = square(x0);
+    double y0y0 = square(y0);
+    double x1x1 = square(x1);
+    double y1y1 = square(y1);
+
+    if (x0x0 + y0y0 >= rr) {
+        return 0;
+    }
+
+    if (x1x1 + y1y1 <= rr) {
+        return (x1 - x0)*(y1 - y0);
+    }
+
+    double xa;
+    double ya;
+    if (x1x1 + y0y0 < rr) {
+        xa = x1;
+        ya = sqrt(rr - x1x1);
+    } else {
+        ya = y0;
+        xa = sqrt(rr - y0y0);
+    }
+
+    double xb;
+    double yb;
+    if (x0x0 + y1y1 < rr) {
+        yb = y1;
+        xb = sqrt(rr - y1y1);
+    } else {
+        xb = x0;
+        yb = sqrt(rr - x0x0);
+    }
+
+    double ta = atan2(ya, xa);
+    double tb = atan2(yb, xb);
+
+    double area = 0.5*(rr*(tb - ta) - xa*yb  + xb*ya)
+        + (xa - x0)*(yb - y0)
+        - 0.5*(xa - xb)*(yb - ya);
+    return area;
+}
+
 double compute_circle_boundary_fraction(
         double xc, double yc, double r,
-        int x, int y, double s)
+        double x0, double y0)
 {
-    double t = std::hypot(x - xc, y - yc);
-    if (r + s <= t) return 0;
-    if (t + s <= r) return 1;
-    double rr = square(r);
-    double ss = square(s);
-    double tt = square(t);
-    if (r + t <= s) return rr/ss;
+    x0 -= xc;
+    y0 -= yc;
 
-    return (ss*std::acos((tt - rr + ss)/(2*s*t)) +
-            rr*std::acos((tt - ss + rr)/(2*r*t)) -
-            0.5*std::sqrt((r + s + t)*(r + s - t)*
-                          (r - s + t)*(s - r + t)))/(M_PI*ss);
+    double rr = r*r;
+
+    double x1 = x0 + 1;
+    double y1 = y0 + 1;
+
+    return circle_quadrant_area(rr, x0, y0, x1, y1) +
+        circle_quadrant_area(rr, -y1, x0, -y0, x1) +
+        circle_quadrant_area(rr, -x1, -y1, -x0, -y0) +
+        circle_quadrant_area(rr, y0, -x1, y1, -x0);
 }
 
 double compute_line_boundary_fraction(
         double a, double b, double c,
-        int x, int y, double s)
+        double x0, double y0)
 {
-    double t = a*x + b*y + c;
-    if (t >= s) return 0;
-    if (t <= -s) return 1;
-    double ss = square(s);
-    double tt = square(t);
-    return (ss*std::acos(t/s) - t*std::sqrt(ss - tt))/(M_PI*ss);
+    double d = a*a + b*b;
+    x0 -= a*c/d;
+    y0 -= b*c/d;
+
+    if (a < 0) {
+        if (b < 0) {
+            x0 = -x0 - 1;
+            y0 = -y0 - 1;
+            a = -a;
+            b = -b;
+        } else {
+            double xt = x0;
+            x0 = y0;
+            y0 = -xt - 1;
+            double t = a;
+            a = b;
+            b = -t;
+        }
+    } else if (b < 0) {
+        double yt = y0;
+        y0 = x0;
+        x0 = -yt - 1;
+        double t = b;
+        b = a;
+        a = -t;
+    }
+
+    if (a*x0 + b*y0 >= 0) {
+        return 0;
+    }
+
+    double x1 = x0 + 1;
+    double y1 = y0 + 1;
+
+    if (a*x1 + b*y1 <= 0) {
+        return 1;
+    }
+
+    double xa;
+    double ya;
+    if (a*x1 + b*y0 <= 0) {
+        xa = x1;
+        ya = -a*x1/b;
+    } else {
+        ya = y0;
+        xa = -b*y0/a;
+    }
+
+    double xb;
+    double yb;
+    if (a*x0 + b*y1 <= 0) {
+        yb = y1;
+        xb = -b*y1/a;
+    } else {
+        xb = x0;
+        yb = -a*x0/b;
+    }
+
+    return (xa - x0)*(yb - y0)
+           - 0.5*(xa - xb)*(yb - ya);
 }
 
 } // namespace
@@ -51,44 +170,44 @@ draw_circle(ImageBuffer<RGBColor>& image,
     int rows = image.rows();
     int cols = image.cols();
 
-    double s = std::sqrt(0.5);
+    double s = sqrt(0.5);
 
-    int ymin{std::max(0, int(std::ceil(yc - (r+s))))};
+    int ymin{max(0, int(ceil(yc - 0.5 - (r+s))))};
     if (ymin >= rows) return;
 
-    int ymax{std::min(rows-1, int(std::floor(yc + (r+s))))};
+    int ymax{min(rows-1, int(floor(yc - 0.5 + (r+s))))};
     if (ymax < 0) return;
 
     RGBColor diff = new_color - old_color;
 
     for (int y = ymin; y <= ymax; ++y) {
-        double d0 = std::sqrt(std::max(0.0, square(r+s) - square(y - yc)));
+        double d0 = sqrt(max(0.0, square(r+s) - square(y - yc + 0.5)));
         double d1;
         if (r > s) {
-            d1 = std::sqrt(std::max(0.0, square(r-s) - square(y - yc)));
+            d1 = sqrt(max(0.0, square(r-s) - square(y - yc + 0.5)));
         } else {
             d1 = 0;
         }
 
-        int xmin1{std::max(0, int(std::ceil(xc - d1)))};
-        int xmax1{std::min(cols-1, int(std::floor(xc + d1)))};
+        int xmin1{max(0, int(ceil(xc - 0.5 - d1)))};
+        int xmax1{min(cols-1, int(floor(xc - 0.5 + d1)))};
 
-        int xmin0{std::max(0, int(std::ceil(xc - d0)))};
-        int xmax0{std::min(cols-1, int(std::floor(xc + d0)))};
+        int xmin0{max(0, int(ceil(xc - 0.5 - d0)))};
+        int xmax0{min(cols-1, int(floor(xc - 0.5 + d0)))};
 
         if (xmin1 < xmax1) {
             for (int x = xmin0; x < xmin1; ++x) {
-                double a = compute_circle_boundary_fraction(xc, yc, r, x, y, s);
+                double a = compute_circle_boundary_fraction(xc, yc, r, x, y);
                 image(y, x) += diff*a;
             }
             image.fill_row(new_color, y, xmin1, xmax1+1);
             for (int x = xmax1+1; x <= xmax0; ++x) {
-                double a = compute_circle_boundary_fraction(xc, yc, r, x, y, s);
+                double a = compute_circle_boundary_fraction(xc, yc, r, x, y);
                 image(y, x) += diff*a;
             }
         } else {
             for (int x = xmin0; x <= xmax0; ++x) {
-                double a = compute_circle_boundary_fraction(xc, yc, r, x, y, s);
+                double a = compute_circle_boundary_fraction(xc, yc, r, x, y);
                 image(y, x) += diff*a;
             }
         }
@@ -103,12 +222,12 @@ void draw_circle_complement(ImageBuffer<RGBColor>& image,
     int rows = image.rows();
     int cols = image.cols();
 
-    double s = std::sqrt(0.5);
+    double s = sqrt(0.5);
 
-    int ymin{std::max(0, int(std::ceil(yc - (r-s))))};
+    int ymin{max(0, int(ceil(yc - 0.5 - (r+s))))};
     if (ymin >= rows) return;
 
-    int ymax{std::min(rows-1, int(std::floor(yc + (r-s))))};
+    int ymax{min(rows-1, int(floor(yc - 0.5 + (r+s))))};
     if (ymax < 0) return;
 
     RGBColor diff = new_color - old_color;
@@ -117,33 +236,33 @@ void draw_circle_complement(ImageBuffer<RGBColor>& image,
         image.fill_row(new_color, y, 0, cols);
     }
     for (int y = ymin; y <= ymax; ++y) {
-        double d0 = std::sqrt(std::max(0.0, square(r+s) - square(y - yc)));
+        double d0 = sqrt(max(0.0, square(r+s) - square(y - yc + 0.5)));
         double d1;
         if (r > s) {
-            d1 = std::sqrt(std::max(0.0, square(r-s) - square(y - yc)));
+            d1 = sqrt(max(0.0, square(r-s) - square(y - yc + 0.5)));
         } else {
             d1 = 0;
         }
 
-        int xmin1{std::max(0, int(std::ceil(xc - d1)))};
-        int xmax1{std::min(cols-1, int(std::floor(xc + d1)))};
+        int xmin1{max(0, int(ceil(xc - 0.5 - d1)))};
+        int xmax1{min(cols-1, int(floor(xc - 0.5 + d1)))};
 
-        int xmin0{std::max(0, int(std::ceil(xc - d0)))};
-        int xmax0{std::min(cols-1, int(std::floor(xc + d0)))};
+        int xmin0{max(0, int(ceil(xc - 0.5 - d0)))};
+        int xmax0{min(cols-1, int(floor(xc - 0.5 + d0)))};
 
         image.fill_row(new_color, y, 0, xmin0);
         if (xmin1 < xmax1) {
             for (int x = xmin0; x < xmin1; ++x) {
-                double a = compute_circle_boundary_fraction(xc, yc, r, x, y, s);
+                double a = compute_circle_boundary_fraction(xc, yc, r, x, y);
                 image(y, x) += diff*(1-a);
             }
             for (int x = xmax1+1; x <= xmax0; ++x) {
-                double a = compute_circle_boundary_fraction(xc, yc, r, x, y, s);
+                double a = compute_circle_boundary_fraction(xc, yc, r, x, y);
                 image(y, x) += diff*(1-a);
             }
         } else {
             for (int x = xmin0; x <= xmax0; ++x) {
-                double a = compute_circle_boundary_fraction(xc, yc, r, x, y, s);
+                double a = compute_circle_boundary_fraction(xc, yc, r, x, y);
                 image(y, x) += diff*(1-a);
             }
         }
@@ -164,69 +283,66 @@ draw_half_plane(ImageBuffer<apollonian::RGBColor>& image,
     int rows = image.rows();
     int cols = image.cols();
 
-    double d = std::hypot(a, b);
+    double d = hypot(a, b);
     a /= d;
     b /= d;
     c /= d;
 
-    double s = std::sqrt(0.5);
-    double c0 = c - s;
-    double c1 = c + s;
-
     RGBColor diff = new_color - old_color;
 
-    if (b < 0) {
-        if (a < 0) {
-            int ymin{std::max(0, int(std::ceil(-(c1 + a*(cols-1))/b)))};
-            int ymax{std::min(rows-1, int(std::floor(-c0/b)))};
-            for (int y = ymin; y <= ymax; ++y) {
-                int xmin0{std::max(0, int(std::ceil(-(c0 + b*y)/a)))};
-                int xmin1{std::min(cols, int(std::floor(-(c1 + b*y)/a)))};
-                for (int x = xmin0; x <= xmin1; ++x) {
-                    double f = compute_line_boundary_fraction(a, b, c, x, y, s);
-                    image(y, x) += diff*f;
-                }
-                image.fill_row(new_color, y, xmin1+1, cols);
-            }
-            image.fill_rect(new_color, ymax+1, rows, 0, cols);
+    if (a == 0) {
+        int y = int(floor(-c/b));
+        if (b < 0) {
+            image.fill_rect(new_color, y+1, rows, 0, cols);
         } else {
-            int ymin{std::max(0, int(std::ceil(-c1/b)))};
-            int ymax{std::min(rows-1, int(std::floor(-(c0 + a*(cols-1))/b)))};
-            for (int y = ymin; y <= ymax; ++y) {
-                int xmin0{std::min(cols, int(std::floor(-(c0 + b*y)/a)))};
-                int xmin1{std::max(0, int(std::ceil(-(c1 + b*y)/a)))};
-                image.fill_row(new_color, y, 0, xmin1);
-                for (int x = xmin1; x <= xmin0; ++x) {
-                    double f = compute_line_boundary_fraction(a, b, c, x, y, s);
+            image.fill_rect(new_color, 0, y, 0, cols);
+        }
+        if (0 <= y && y < rows) {
+            for (int x = 0; x < cols; ++x) {
+                double f = compute_line_boundary_fraction(a, b, c, x, y);
+                image(y, x) += diff*f;
+            }
+        }
+    } else if (a < 0) {
+        if (b < 0) {
+            for (int y = 0; y < rows; ++y) {
+                int x0 = max(0, int(floor(-(c + b*(y+1))/a)));
+                int x1 = min(cols, int(ceil(-(c + b*y)/a)));
+                for (int x = x0; x < x1; ++x) {
+                    double f = compute_line_boundary_fraction(a, b, c, x, y);
                     image(y, x) += diff*f;
                 }
+                image.fill_row(new_color, y, x1, cols);
             }
-            image.fill_rect(new_color, ymax+1, rows, 0, cols);
+        } else {
+            for (int y = 0; y < rows; ++y) {
+                int x0 = max(0, int(floor(-(c + b*y)/a)));
+                int x1 = min(cols, int(ceil(-(c + b*(y+1))/a)));
+                for (int x = x0; x < x1; ++x) {
+                    double f = compute_line_boundary_fraction(a, b, c, x, y);
+                    image(y, x) += diff*f;
+                }
+                image.fill_row(new_color, y, x1, cols);
+            }
         }
     } else {
-        if (a < 0) {
-            int ymin{std::max(0, int(std::ceil(-c1/b)))};
-            int ymax{std::min(rows-1, int(std::floor(-(c0 + a*(cols-1))/b)))};
-            image.fill_rect(new_color, 0, ymin, 0, cols);
-            for (int y = ymin; y <= ymax; ++y) {
-                int xmin0{std::max(0, int(std::ceil(-(c0 + b*y)/a)))};
-                int xmin1{std::min(cols, int(std::floor(-(c1 + b*y)/a)))};
-                for (int x = xmin0; x <= xmin1; ++x) {
-                    double f = compute_line_boundary_fraction(a, b, c, x, y, s);
+        if (b < 0) {
+            for (int y = 0; y < rows; ++y) {
+                int x0 = max(0, int(floor(-(c + b*y)/a)));
+                int x1 = min(cols, int(ceil(-(c + b*(y+1))/a)));
+                image.fill_row(new_color, y, 0, x0);
+                for (int x = x0; x < x1; ++x) {
+                    double f = compute_line_boundary_fraction(a, b, c, x, y);
                     image(y, x) += diff*f;
                 }
-                image.fill_row(new_color, y, xmin1+1, cols);
             }
         } else {
-            int ymin{std::max(0, int(std::ceil(-(c1 + a*(cols-1))/b)))};
-            int ymax{std::min(rows-1, int(std::floor(-c0/b)))};
-            image.fill_rect(new_color, 0, ymin, 0, cols);
-            for (int y = ymin; y <= ymax; ++y) {
-                int xmin0{std::min(cols, int(std::floor(-(c0 + b*y)/a)))};
-                int xmin1{std::max(0, int(std::ceil(-(c1 + b*y)/a)))};
-                image.fill_row(new_color, y, 0, xmin1);
-                for (int x = xmin1; x <= xmin0; ++x) {
-                    double f = compute_line_boundary_fraction(a, b, c, x, y, s);
+            for (int y = 0; y < rows; ++y) {
+                int x0 = max(0, int(floor(-(c + b*(y+1))/a)));
+                int x1 = min(cols, int(ceil(-(c + b*y)/a)));
+                image.fill_row(new_color, y, 0, x0);
+                for (int x = x0; x < x1; ++x) {
+                    double f = compute_line_boundary_fraction(a, b, c, x, y);
                     image(y, x) += diff*f;
                 }
             }
