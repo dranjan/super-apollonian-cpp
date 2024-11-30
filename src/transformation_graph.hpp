39c257539c8fa0fb8df3f7d2f190af1383ad5d57
Darsh Ranjan <dranjan@berkeley.edu>
2024-11-29 22:03:43 -0800

    Add some comments

--- src/transformation_graph.hpp
+++ src/transformation_graph.hpp
@@ -8,9 +8,11 @@ namespace apollonian {
 
 template <typename Transform>
 struct graph_edge {
-    unsigned int type_index;
-    unsigned int id;
-    Transform transform;
+    unsigned int type_index;  /* The index of the target node. */
+    unsigned int id;          /* An easily hashable or comparable identifier
+                               * for this edge or transformation.
+                               */
+    Transform transform;      /* The transformation itself. */
 };
 
 template <unsigned int N, typename Transform>
@@ -20,6 +22,7 @@ public:
     transformation_graph(Args&&... args);
 
 public:
+    /* The adjacency list of the transformation graph. */
     std::array<std::vector<graph_edge<Transform>>, N> edges_;
 };
 
