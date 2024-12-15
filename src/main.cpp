5cd6ad62e395d5b417fe07207cd3352b69a000fe
Darsh Ranjan <dranjan@berkeley.edu>
2024-12-15 00:07:21 -0800

    Apply unsharp mask

--- src/main.cpp
+++ src/main.cpp
@@ -9,6 +9,7 @@
 #include <iostream>
 #include <string>
 
+#include "filters.hpp"
 #include "io.hpp"
 #include "visitor.hpp"
 
@@ -29,14 +30,23 @@ int main(int argc, char* argv[]) {
     rgb_color c2(0.0, 0.6, 1.0);
     rgb_color c3(1.0, 0.6, 0.0);
 
+    /* For convenience and testing, all post-filtering can be enabled or
+     * disabled via this one boolean.
+     */
+    bool use_filters = true;
+
+    unsharp_mask filter(5.0, 1.0);
+
+    int padding = use_filters? filter.padding() : 0;
+
     /* These values can be increased to reduce computation for quicker
      * testing. Set both to 1 for the full rendering.
      */
     int scale_down = 1;           /* Increase to make a smaller image. */
     double threshold_factor = 1;  /* Increase to use fewer circles. */
 
-    size_t w = 3840 / scale_down;
-    size_t h = 2160 / scale_down;
+    size_t w = 3840 / scale_down + 2*padding;
+    size_t h = 2160 / scale_down + 2*padding;
     double res = 1000 / scale_down;
     rgb_color bgcolor = rgb_color::black;
     renderer renderer_(w, h, dcomplex(-2.4, -2.0), res);
@@ -56,7 +66,14 @@ int main(int argc, char* argv[]) {
     rendering_grid grid(num_threads, a, b, c, cell_size, cell_size, visitor);
     grid.run();
 
-    save_image(visitor.buffer(), filename);
+    if (use_filters) {
+        std::cout << "applying post-processing filters..." << std::endl;
+        auto image = filter.apply(visitor.buffer());
+        std::cout << "done." << std::endl;
+        save_image(image, filename);
+    } else {
+        save_image(visitor.buffer(), filename);
+    }
 
     return 0;
 }
